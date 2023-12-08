53b4d42c3745061690280bd2da5c6405ffe098f9
sstein31 <89949607+sstein31@users.noreply.github.com>
2023-12-08 15:28:17 -0500

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ def main():
 
     # define loss function (criterion) and optimizer
     # cosine similarity between embeddings -> input1, input2, target
-    cosine_crit = nn.MSEloss().to(device)
+    cosine_crit = nn.MSELoss().to(device)
     # cosine_crit = nn.CosineEmbeddingLoss(0.1).to(device)
     if opts.semantic_reg:
         weights_class = torch.Tensor(opts.numClasses).fill_(1)
